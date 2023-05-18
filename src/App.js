79b653c671698d923381241b7e88cfd9153784d8
m0i5 <111139099+m0i5@users.noreply.github.com>
2023-05-18 18:40:06 +0200

    Component for each movie

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import {useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg'
 
 
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9309afd6"
+
+const movie1 = 
+    {
+        "Title": "The Amazing Spiderman 2 Webb Cut",
+        "Year": "2021",
+        "imdbID": "tt18351128",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
+      }
+
 
 const App = () => {
     const searchMovies = async(title) => {
@@ -18,7 +30,30 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className="app">
+<h1>CineSearch</h1>
+<div className="search">
+    <input
+     placeholder="Search for movies"
+     value="Superman"
+     onChange={() => {}}
+     
+     
+      />
+      <img
+      src={SearchIcon} 
+        alt='search'
+        onClick={() => {}}
+      />
+</div>
+
+<div className="container">
+<MovieCard />
+</div>
+
+
+
+        </div>
     )
 }
 
